877c5bf3f237ed398801f584349e0a86289dacf7
tmoreno <tmorenobernal@yahoo.es>
2016-05-28 13:30:53 +0200

    Added code for pass the test

--- src/main/java/com/tmoreno/kata/bowling/LineScore.java
+++ src/main/java/com/tmoreno/kata/bowling/LineScore.java
@@ -3,6 +3,7 @@ package com.tmoreno.kata.bowling;
 public class LineScore {
 
 	private static final String MISS_SYMBOL = "-";
+	private static final String SPARE_SYMBOL = "/";
 
 	private String[] line;
 
@@ -14,7 +15,11 @@ public class LineScore {
 		int score = 0;
 
 		for (int i = 0; i < line.length; i++) {
-			if (!MISS_SYMBOL.equals(line[i])) {
+			if (SPARE_SYMBOL.equals(line[i])) {
+				score += 10 + Integer.parseInt(line[i + 1])
+						- Integer.parseInt(line[i - 1]);
+			}
+			else if (!MISS_SYMBOL.equals(line[i])) {
 				score += Integer.parseInt(line[i]);
 			}
 		}
