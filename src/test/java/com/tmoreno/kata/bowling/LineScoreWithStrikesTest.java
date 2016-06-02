0a6229f451b072fd7cee79637978ffb4ddf786ad
tmoreno <tmorenobernal@yahoo.es>
2016-06-02 23:12:31 +0200

    Removed old code

--- src/test/java/com/tmoreno/kata/bowling/LineScoreWithStrikesTest.java
+++ src/test/java/com/tmoreno/kata/bowling/LineScoreWithStrikesTest.java
@@ -8,8 +8,7 @@ import org.junit.Test;
 
 public class LineScoreWithStrikesTest {
 
-	private String line;
-	private List<Roll> line2;
+	private List<Roll> line;
 	private LineParser lineParser;
 	private LineScore lineScore;
 
@@ -20,36 +19,36 @@ public class LineScoreWithStrikesTest {
 
 	@Test
 	public void whenGetAStrikeAddNextTwoRollsKnockedDownPins() {
-		line2 = lineParser.parse("X11----------------");
+		line = lineParser.parse("X11----------------");
 
-		lineScore = new LineScore(line2);
+		lineScore = new LineScore(line);
 
 		Assert.assertEquals(14, lineScore.calc());
 	}
 
 	@Test
 	public void whenGetAStrikeAtTheEndGetTwoExtraThrow() {
-		line2 = lineParser.parse("------------------X11");
+		line = lineParser.parse("------------------X11");
 
-		lineScore = new LineScore(line2);
+		lineScore = new LineScore(line);
 
 		Assert.assertEquals(12, lineScore.calc());
 	}
 
 	@Test
 	public void twoStrikes() {
-		line2 = lineParser.parse("XX------------------");
+		line = lineParser.parse("XX------------------");
 
-		lineScore = new LineScore(line2);
+		lineScore = new LineScore(line);
 
 		Assert.assertEquals(30, lineScore.calc());
 	}
 
 	@Test
 	public void complex() {
-		line2 = lineParser.parse("XXXXXXXXXXXX");
+		line = lineParser.parse("XXXXXXXXXXXX");
 
-		lineScore = new LineScore(line2);
+		lineScore = new LineScore(line);
 
 		Assert.assertEquals(300, lineScore.calc());
 	}
